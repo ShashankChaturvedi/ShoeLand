2ade70c5378aa99aa026380c259c7ff3241f483f
ShashankChaturvedi <gshashank1998@gmail.com>
2023-02-10 23:52:41 +0530

    shsfh

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const path=require("path");
 
 //config
 if (process.env.NODE_ENV !== "PRODUCTION") {
-    require("dotenv").config({ path: "backend/config/config.env" });
+    require("dotenv").config({ path: "config/config.env" });
 }
 
 
